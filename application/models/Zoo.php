c3b380d4790933a51a92a95187cfade00d1e1f5b
Anthony Wong <awong6201@gmail.com>
2018-03-08 15:32:54 -0800

    Added business logic for Zoo and Monkey models

--- application/models/Zoo.php
+++ application/models/Zoo.php
@@ -1,4 +1,11 @@
 <?php
 
-class Zoo extends Memory_Model {    
+class Zoo extends Memory_Model {
+    
+    // over-ride base collection adding, with a limit
+    function add($record) {
+        if ($this->size() >= 6) {
+            throw new Exception('The zoo is full');
+        }
+    }
 }
